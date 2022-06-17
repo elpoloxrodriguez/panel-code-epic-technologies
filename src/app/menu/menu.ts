b11565c97653ffdeb756fa481bc6254c907974b8
ANDRES RODRIGUEZ <elpoloxrodriguez@gmail.com>
2022-06-17 14:20:56 -0400

    Consola Code-Epic Technologies

---
+++ src/app/menu/menu.ts
@@ -0,0 +1,164 @@
+  import { CoreMenu } from '@core/types';
+
+  //? DOC: http://localhost:7777/demo/vuexy-angular-admin-dashboard-template/documentation/guide/development/navigation-menus.html#interface
+  
+  export const menu: CoreMenu[] = [
+    // Dashboard
+    {
+      id: 'home',
+      nombre: 'Escritorio',
+      icono: 'home',
+      type: 'item',
+      url: 'home',
+    },
+  
+    // Others
+    // {
+    //   id: 'invoice',
+    //   nombre: 'Redes',
+    //   translate: 'MENU.APPS.INVOICE.COLLAPSIBLE',
+    //   type: 'collapsible',
+    //   icono: 'slack',
+    //   children: [
+    //     {
+    //       id: 'invoice-list',
+    //       nombre: 'Comunicaciones',
+    //       translate: 'MENU.APPS.INVOICE.LIST',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/list'
+    //     },
+    //     {
+    //       id: 'invoicePreview',
+    //       nombre: 'Conexiones',
+    //       translate: 'MENU.APPS.INVOICE.PREVIEW',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/preview'
+    //     },
+    //     {
+    //       id: 'invoiceEdit',
+    //       nombre: 'Monitoreo',
+    //       translate: 'MENU.APPS.INVOICE.EDIT',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/edit'
+    //     }
+    //   ]
+    // },
+    // {
+    //   id: 'invoice',
+    //   nombre: 'Herramienta',
+    //   translate: 'MENU.APPS.INVOICE.COLLAPSIBLE',
+    //   type: 'collapsible',
+    //   icono: 'tool',
+    //   children: [
+    //     {
+    //       id: 'invoice-list',
+    //       nombre: 'Funciones',
+    //       translate: 'MENU.APPS.INVOICE.LIST',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/list'
+    //     },
+    //     {
+    //       id: 'invoicePreview',
+    //       nombre: 'API',
+    //       translate: 'MENU.APPS.INVOICE.PREVIEW',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/preview'
+    //     },
+    //     {
+    //       id: 'invoiceEdit',
+    //       nombre: 'GraphQL',
+    //       translate: 'MENU.APPS.INVOICE.EDIT',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/edit'
+    //     },
+    //     {
+    //       id: 'invoiceAdd',
+    //       nombre: 'WorkFlow',
+    //       translate: 'MENU.APPS.INVOICE.ADD',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/add'
+    //     }
+    //   ]
+    // },
+    // {
+    //   id: 'invoice',
+    //   nombre: 'Aplicaciones',
+    //   translate: 'MENU.APPS.INVOICE.COLLAPSIBLE',
+    //   type: 'collapsible',
+    //   icono: 'server',
+    //   children: [
+    //     {
+    //       id: 'invoicePreview',
+    //       nombre: 'Instalar APP',
+    //       translate: 'MENU.APPS.INVOICE.PREVIEW',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/preview'
+    //     },
+    //     {
+    //       id: 'invoiceEdit',
+    //       nombre: 'Definir Menu',
+    //       translate: 'MENU.APPS.INVOICE.EDIT',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/edit'
+    //     },
+    //     {
+    //       id: 'invoiceAdd',
+    //       nombre: 'Monitoreo',
+    //       translate: 'MENU.APPS.INVOICE.ADD',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/add'
+    //     },{
+    //       id: 'invoiceAdd',
+    //       nombre: 'Eventos',
+    //       translate: 'MENU.APPS.INVOICE.ADD',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/add'
+    //     }
+    //   ]
+    // },
+    // {
+    //   id: 'invoice',
+    //   nombre: 'Seguridad',
+    //   translate: 'MENU.APPS.INVOICE.COLLAPSIBLE',
+    //   type: 'collapsible',
+    //   icono: 'lock',
+    //   children: [
+    //     {
+    //       id: 'invoicePreview',
+    //       nombre: 'Roles',
+    //       translate: 'MENU.APPS.INVOICE.PREVIEW',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/preview'
+    //     },
+    //     {
+    //       id: 'invoiceEdit',
+    //       nombre: 'Perfiles',
+    //       translate: 'MENU.APPS.INVOICE.EDIT',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/edit'
+    //     },
+    //     {
+    //       id: 'invoiceAdd',
+    //       nombre: 'Perfiles',
+    //       translate: 'MENU.APPS.INVOICE.ADD',
+    //       type: 'item',
+    //       icono: 'circle',
+    //       url: 'apps/invoice/add'
+    //     }
+    //   ]
+    // },
+  ];
+  
